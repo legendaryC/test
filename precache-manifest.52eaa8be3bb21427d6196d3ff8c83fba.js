a345ce439231431f3846d6b8917c98738ed6952d
Chen Jian <chenjian@Chens-MBP.student.uwlax.edu>
2020-01-20 22:22:23 -0600

    Updates

---
+++ precache-manifest.52eaa8be3bb21427d6196d3ff8c83fba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0103945073a29ba234fec492eee7cc52",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "321c226ba7a594a0ffac",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bd07b41fd8b200aa0983",
+    "url": "/test/static/js/2.ed930f27.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test/static/js/2.ed930f27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "321c226ba7a594a0ffac",
+    "url": "/test/static/js/main.7da28aaa.chunk.js"
+  },
+  {
+    "revision": "25efeaacbead4a84b640",
+    "url": "/test/static/js/runtime-main.909a2939.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
